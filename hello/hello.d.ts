591979ca753441f42db0e8f7a4c9c2113a0cbfbf
Vitalij Reicherdt <vitalij.reicherdt@commerzbank.com>
2017-08-22 14:37:36 +0200

    doing

---
+++ hello/hello.d.ts
@@ -0,0 +1,3 @@
+declare const s3: string;
+declare const a = 1;
+declare const s4: string;
---
+++ hello/student.d.ts
@@ -0,0 +1,6 @@
+export declare class Student {
+    private name;
+    private num;
+    constructor(name: string, num: number);
+    getLabel(): string;
+}
